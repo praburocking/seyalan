974e72410c30ee129a56a0a1e97e75b776f771b4
prabu.mohan <prabumohan96@gmail.com>
2021-06-22 02:52:42 +0530

    profile and permission added

---
+++ profile_and_permission/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProfileAndPermissionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'profile_and_permission'
---
+++ profile_and_permission/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from tenant_user_handle.models import Org,User
+# Create your models here.
+
+class Permission(models.Model):
+    id=models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False)
+    name=models.CharField(max_length=250)
+    sys_ref_name=models.CharField(max_length=250)
+    desc=models.TextField(null=True)
+
+class Profile(models.Model):
+    id=models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False)
+    name=models.CharField(max_length=250)
+    sys_ref_name=models.CharField(max_length=250)
+    desc=models.TextField(null=True)
+    users=models.ManyToManyField(User)
+    permissions=models.ManyToManyField(Permission)
+
+
+
+
+
