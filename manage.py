ba4e7bfc48ef4e6cc7152d679923a69e0367d009
prabu.mohan <prabumohan96@gmail.com>
2021-07-04 03:04:29 +0530

    folder names updated

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iam.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seyalan.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
