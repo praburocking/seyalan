4d489767eab4fd4d4bbcd6cd4af829bfde479c03
prabu.mohan <prabumohan96@gmail.com>
2021-07-04 03:12:04 +0530

    test

--- profile_and_permission/models.py
+++ profile_and_permission/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from tenant_user_handle.models import Org,User
 import uuid
-# Create your models here.
+# Create your models here. tessts
 
 class Permission(models.Model):
     id=models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False)
